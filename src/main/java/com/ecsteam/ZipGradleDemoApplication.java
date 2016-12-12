5994c94149a42ecba9f3254c0be40c579d583be9
Josh Ghiloni <jghiloni@ecsteam.com>
2016-12-12 15:48:19 -0700

    initial commit

---
+++ src/main/java/com/ecsteam/ZipGradleDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ecsteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipGradleDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ecsteam/ZipGradleDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecsteam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipGradleDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
